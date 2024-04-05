3fd192d3604bec0ce44f1f9038d981fab8352b76
BradleyDev8 <139467582+BradleyDev8@users.noreply.github.com>
2024-04-05 22:07:36 +0100

    Added more emails to schema

--- utils/emailSchema.ts
+++ utils/emailSchema.ts
@@ -3,8 +3,14 @@ import { z } from "zod";
 const emailSchema = z
   .string()
   .refine(
-    (email) => email.endsWith("@gmail.com") || email.endsWith("@outlook.com"),
-    "Only Gmail and Outlook emails are allowed."
+    (email) =>
+      email.endsWith("@gmail.com") ||
+      email.endsWith("@outlook.com") ||
+      email.endsWith("@hotmail.com") ||
+      email.endsWith("@outlook.co.uk") ||
+      email.endsWith("@gmail.co.uk") ||
+      email.endsWith("@live.com") ||
+      "Only Gmail and Outlook emails are allowed."
   );
 
 export default emailSchema;
